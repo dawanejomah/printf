3ade44e0f934bb282912f6cc903fa2f00c3dcd37
Dawan Ejomah <dawanejomah@gmail.com>
2023-08-22 07:21:14 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+  *_printf - formats a string and print the string
+  *@format: the string format
+  *Return: 0 on success and -1 on error
+  */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0, j = 0;
+
+	va_start(ap, format);
+	if (format)
+	{
+		while (format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				while (format[i + 1] == ' ')
+					i++;
+				if (format[i + 1] == '\0')
+					return (-1);
+				j += print_fstring(&i, format, ap);
+				i += 2;
+				continue;
+			}
+			_putchar(format[i]);
+			i++;
+			j++;
+		}
+	}
+	else
+		return (-1);
+	va_end(ap);
+	return (j);
+}
---
+++ main.h
@@ -0,0 +1,41 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_fstring(int *pos, const char *format, va_list ap);
+int print_string(va_list ap);
+int print_char(va_list ap);
+int print_percent(va_list ap);
+int print_int(va_list ap);
+int print_unsigned_int(va_list ap);
+int print_octal_number(va_list ap);
+int print_hexadecimal(va_list ap);
+int print_hexadecimal_cap(va_list ap);
+int print_hexadecimal_all(va_list ap, char c);
+
+/**
+ * struct conv_spec - Struct op
+ *
+ * @spec: The specifier.
+ * @func: The function associated with the specifier.
+ */
+struct conv_spec
+{
+	char spec;
+	int (*func)(va_list);
+};
+
+/**
+ * typedef struct conv_spec conv_spec_t - struct op
+ *
+ * @conv_spec: The struct.
+ * @conv_spec_t: The name associated to the struct.
+ */
+typedef struct conv_spec conv_spec_t;
+
+#endif 
